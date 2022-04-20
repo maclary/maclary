b896c98c810504da1c79a1a36275bf1b59690e2b
ApteryxXYZ <apteryx.xyz@gmail.com>
2022-04-20 22:35:14 +1200

    refactor(Preconditions): reword error messages

--- src/preconditions/DMOnly.ts
+++ src/preconditions/DMOnly.ts
@@ -4,16 +4,16 @@ import type { Command } from '../structures/Command';
 export class DMOnly extends Precondition {
     public override messageRun(message: Command.Message): Result {
         if (message.guildId === null) return this.ok();
-        return this.error('You cannot run this message command outside of DMs.');
+        return this.error('You cannot run this command outside of DMs.');
     }
 
     public override chatInputRun(interaction: Command.ChatInput): Result {
         if (interaction.guildId === null) return this.ok();
-        return this.error('You cannot run this chat input command outside of DMs.');
+        return this.error('You cannot run this command outside of DMs.');
     }
 
     public override contextMenuRun(interaction: Command.ContextMenu): Result {
         if (interaction.guildId === null) return this.ok();
-        return this.error('You cannot run this context menu command outside of DMs.');
+        return this.error('You cannot run this command outside of DMs.');
     }
 }
